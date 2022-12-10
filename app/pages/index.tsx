454f401fca70fc838df64918c8f3ea3ce579d41e
lebrancconvas <lebrancconvas@gmail.com>
2022-12-10 17:43:22 +0700

    ✨ feat: create new page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,8 +3,24 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+    
     <div>
-      <h1>Hello World</h1> 
+      <Head> 
+        <title>First Page</title> 
+      </Head>
+      <header>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </header> 
+      <section>
+        <div>
+          <h2>My First Post</h2>
+        </div>
+        <div>
+          <p>Posted on 1/1/2021</p> 
+        </div>
+      </section>
     </div>
   )
 }
---
+++ app/pages/users.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Users() {
+	return(
+		<div>
+			<Head>
+				<title>Users</title>
+			</Head>
+			<header>
+				<div>
+					<h1>Users</h1> 
+				</div>
+			</header>
+		</div>
+	)
+}
